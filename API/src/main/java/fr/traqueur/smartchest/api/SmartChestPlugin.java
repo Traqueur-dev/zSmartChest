11e47aa369a03e481e76c4e7484af88b54ec3fab
Traqueur-dev <traqueur.dev@gmail.com>
2024-11-14 11:40:07 +0100

    fix: general structure

---
+++ API/src/main/java/fr/traqueur/smartchest/api/SmartChestPlugin.java
@@ -0,0 +1,6 @@
+package fr.traqueur.smartchest.api;
+
+import fr.groupez.api.ZPlugin;
+
+public abstract class SmartChestPlugin extends ZPlugin {
+}
---
+++ src/main/java/fr/tarqueur/smartchest/ZSmartChest.java
@@ -0,0 +1,17 @@
+package fr.tarqueur.smartchest;
+
+import fr.traqueur.smartchest.api.SmartChestPlugin;
+
+public final class ZSmartChest extends SmartChestPlugin {
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void disable() {
+
+    }
+
+}
